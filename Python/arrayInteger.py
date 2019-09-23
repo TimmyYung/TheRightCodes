3f34297382d24b2d87d10977255463a724340fa8
TimmyYung <592619@pdsb.net>
2019-09-23 15:16:10 -0400

    Update Python Files

--- Python/arrayInteger.py
+++ Python/arrayInteger.py
@@ -1,3 +1,6 @@
-list1 = [1, 2, "3", 4, "5", 10, "12"] # List of strings and integers
-list1 = [int(i) for i in list1] # Converts each item in the list to an integer
-print(list1) # Prints out the list of integers
+def converter(list2):
+    list2 = [int(i) for i in list2] # Converts each item in the list to an integer
+    list3 = sum(list2) # Adds the numbers in the list
+    return list3
+
+print(converter([1, 2, "3", 4, "5", 10, "12"]))
--- Python/sumString.py
+++ Python/sumString.py
@@ -1,5 +1,6 @@
-S = "1, 4, 2, 6, 3, 7" # String with numbers
-spliter = S.split(",") # Split all of the numbers individually
-list1 = [int(i) for i in spliter] # Convert the string into integers
-adder = sum(list1) # Add all of the intergers
-print(adder) # Prints the results of the addition
+def sorter(spliter):
+    slicer = spliter.split(",") # Split all of the numbers individually
+    list1 = [int(i) for i in slicer] # Convert the string into integers
+    adder = sum(list1) # Add all of the integers
+    return adder
+print(sorter("1, 4, 2, 6, 3, 7")) # Prints the results of the addition
