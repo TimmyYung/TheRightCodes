944faf27bbf563d465600ac9837f6fc0f378619e
TimmyYung <592619@pdsb.net>
2019-10-20 00:24:28 -0400

    Create multiples.py

---
+++ Python/multiples.py
@@ -0,0 +1,11 @@
+def is_multiple(a, b):
+    if (a%b == 0):
+        print("True")
+    else:
+        print("False")
+
+is_multiple(12, 3)
+is_multiple(12, 4)
+is_multiple(12, 5)
+is_multiple(12, 6)
+is_multiple(12, 7)
